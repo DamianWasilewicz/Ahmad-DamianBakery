a68e8c7c756be3f9cda71b30cdeec6723be155fb
Damian Wasilewicz <damian_wasilewicz@brown.edu>
2022-09-10 17:28:27 -0400

    added stuff

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = 8010;
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -29,7 +29,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -40,14 +40,31 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const unhealthy = (fruit) => {
+    return fruit.nutritions.sugar > 10;
+  }
 
+  const unhealthyFruit = fruitsArray.filter(unhealthy).map(function (fruit) { return '<li>' + fruit.name + '</li>'});
+
+  console.log(unhealthyFruit);
   // TODO: Create a new HTML element to display your data 
+  const newP = document.createElement("p");
+  newP.innerHTML = "Fruit with more than 10 sugar";
+
+  const newDiv = document.createElement("ul");
+  newDiv.innerHTML = unhealthyFruit.join('');
+  newDiv.display = "flex";
+
+
 
   // TODO: Append your new element to the page
+  const existingElement = document.getElementById("cs1300-gallery");
+  existingElement.append(newP);
+  existingElement.append(newDiv);
 
 }
 
@@ -60,4 +77,5 @@ const exampleAddElement = () => {
   // Append the new element to an existing part of the webpage
   const existingElement = document.getElementById('example-id');
   existingElement.append(newElement);
+
 }
